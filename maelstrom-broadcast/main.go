4bd7c2a5e4d18b79ef3694c26c3097a5ca83534d
rjames187 <rory.james2021@gmail.com>
2024-09-07 17:19:47 -0500

    complete multi-node broadcast challenge

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -7,24 +7,41 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+var seen = []float64{}
+var topology = map[string]any{}
+var n = maelstrom.NewNode()
+
+func broadcastHandler(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+	}
+
+	value := body["message"].(float64)
+
+	// check if this node already received the broadcast
+	for _, v := range seen {
+		if value == v {
+			return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
+		}
+	}
+
+	seen = append(seen, value)
+
+	for node, neighbors := range topology {
+		if node == msg.Dest {
+			for _, neighbor := range neighbors.([]any) {
+				n.RPC(neighbor.(string), body, broadcastHandler)
+			}
+			break
+		}
+	}
+
+	return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
+}
+
 func main() {
-	n := maelstrom.NewNode()
-
-	seen := []float64{}
-
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-    var body map[string]any
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-        return err
-    }
-
-		seen = append(seen, body["message"].(float64))
-
-    body["type"] = "broadcast_ok"
-		delete(body, "message")
-
-    return n.Reply(msg, body)
-	})
+	n.Handle("broadcast", broadcastHandler)
 
 	n.Handle("read", func(msg maelstrom.Message) error {
     var body map[string]any
@@ -43,6 +60,8 @@ func main() {
     if err := json.Unmarshal(msg.Body, &body); err != nil {
         return err
     }
+
+		topology = body["topology"].(map[string]any)
 
     body["type"] = "topology_ok"
 		delete(body, "topology")
