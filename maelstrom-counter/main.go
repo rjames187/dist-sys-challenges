8dc7f3e61702f259f2cd6bd0b76e2948236ec196
rjames187 <rory.james2021@gmail.com>
2024-09-10 21:55:32 -0500

    attempt grow only counter challenge (failed)

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -10,56 +10,43 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type replica struct {
-	dest string
-	delta int
-}
-
 func main() {
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
 	ctx := context.Background()
 
-	q := []replica{}
+	q := []int{}
 	mu := &sync.Mutex{}
 
-	countStarted := false
+	startedCount := false
 
 	go func() {
 		for {
 			time.Sleep(time.Millisecond * 300)
 
-			newQ := []replica{}
-			wg := &sync.WaitGroup{}
-			newQMu := &sync.Mutex{}
-
 			mu.Lock()
-			for _, data := range q {
-				wg.Add(1)
-				go func(rep replica) {
-					defer wg.Done()
-					_, err := n.SyncRPC(ctx, rep.dest, map[string]any{"type": "replicate", "delta": rep.delta})
-					if err != nil {
-						newQMu.Lock()
-						newQ = append(newQ, rep)
-						newQMu.Unlock()
-					}
-				}(data)
+			delta := 0
+			for _, num := range q {
+				delta += num
 			}
+			count, err := kv.ReadInt(ctx, "count")
+			if err != nil {
+				continue
+			}
+			err = kv.CompareAndSwap(ctx, "count", count, count + delta, true)
+			if err != nil {
+				continue
+			}
+			q = []int{}
 			mu.Unlock()
-			wg.Wait()
-			q = newQ
 		}
 	}()
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		mu.Lock()
-		if !countStarted {
-			err := kv.CompareAndSwap(ctx, "count", 0, 0, true)
-			if err != nil {
-				return err
-			}
-			countStarted = true
+		if !startedCount {
+			kv.CompareAndSwap(ctx, "count", 0, 0, true)
+			startedCount = true
 		}
 		mu.Unlock()
 
@@ -72,31 +59,10 @@ func main() {
 		delta := int(body["delta"].(float64))
 		
 		mu.Lock()
-		for _, node := range n.NodeIDs() {
-			q = append(q, replica{node, delta})
-		}
+		q = append(q, delta)
 		mu.Unlock()
 
 		return n.Reply(msg, map[string]any{"type": "add_ok"})
-	})
-
-	n.Handle("replicate", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		delta := int(body["delta"].(float64))
-		count, err := kv.ReadInt(ctx, "count")
-		if err != nil {
-			return err
-		}
-		err = kv.Write(ctx, "count", delta + count)
-		if err != nil {
-			return err
-		}
-
-		return n.Reply(msg, map[string]any{"type": "replicate_ok"})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
